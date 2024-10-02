5aa9fd3b9de0b0da7d2e5d9f1ca11ed8744c93d2
Ayesha788 <164652720+Ayesha788@users.noreply.github.com>
2024-10-02 22:30:33 +0500

    Add files via upload

---
+++ src/app/page.tsx
@@ -0,0 +1,22 @@
+
+
+export default function Home() {
+  return (
+    <>
+      <div className="homeContainer>">
+        <div className="childContainer">
+          Hey,
+          <img  id="profilePic" src="/profilePic.jpg"></img>
+
+          
+          <br />
+          My Name is <span className="pinkColor">Ayesha</span>
+          <br />I am Next Js Developer
+        </div>
+        <div className="childContainer"></div>
+      </div>
+
+      
+    </>
+  );
+}
